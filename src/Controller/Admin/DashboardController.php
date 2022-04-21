412388fa23837a1ed0bfaed27fff41e07a0396a3
Far-Traveller <carriere.aurore@orange.fr>
2022-04-21 10:41:19 +0200

    added UserCrudController

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Formation;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -28,5 +30,9 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::section('Users');
+        yield MenuItem::linkToCrud('Users', 'fas fa-user', User::class);
+//        yield MenuItem::section('Formations');
+//        yield MenuItem::linkToCrud('Formations', 'fas fa-user', Formation::class);
     }
 }
