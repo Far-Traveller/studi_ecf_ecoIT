62ef58979031bea4e296a204697cbf1b914f4b99
Far-Traveller <carriere.aurore@orange.fr>
2022-04-21 06:28:33 +0200

    added list lessons done in profile learner

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,17 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\LessonRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
-    #[Route('/profil', name: 'app_learner_profile')]
-    public function index(): Response
+    private UserRepository $userRepo;
+    private LessonRepository $lessonRepo;
+
+    public function __construct(UserRepository $userRepo, LessonRepository $lessonRepo)
     {
+        $this->userRepo = $userRepo;
+        $this->lessonRepo = $lessonRepo;
+    }
+
+
+    #[Route('/profil/{id}', name: 'app_learner_profile')]
+    public function index(User $user): Response
+    {
+        $done_lessons = $user->getDoneLessons();
+        //$done_lessons = $this->lessonRepo->findBy(['doneLessons' => $id_user]);
+
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
+            'done_lessons' => $done_lessons
         ]);
     }
 
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
@@ -34,6 +36,14 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\Column(type: 'string', length: 255)]
     private $lastname;
+
+    #[ORM\ManyToMany(targetEntity: Lesson::class)]
+    private $DoneLessons;
+
+    public function __construct()
+    {
+        $this->DoneLessons = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -137,6 +147,30 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setLastname(string $lastname): self
     {
         $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Lesson>
+     */
+    public function getDoneLessons(): Collection
+    {
+        return $this->DoneLessons;
+    }
+
+    public function addDoneLesson(Lesson $doneLesson): self
+    {
+        if (!$this->DoneLessons->contains($doneLesson)) {
+            $this->DoneLessons[] = $doneLesson;
+        }
+
+        return $this;
+    }
+
+    public function removeDoneLesson(Lesson $doneLesson): self
+    {
+        $this->DoneLessons->removeElement($doneLesson);
 
         return $this;
     }
